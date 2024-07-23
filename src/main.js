a9158fa1ee08e20e861b5465228e19c3fcfdb019
Eduardo Morais <156036079+edrdudi@users.noreply.github.com>
2024-07-23 11:34:38 -0300

    Entrega do Desafio de Lógica da DIO

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-console.log("Olá mundo!")
+let heroName = "Valkios"
+let heroXP = 10002
+let heroRank
 
-console.log("Olá")
+for (let i = 0; i <= 1; i++) {
+
+  if (heroXP <= 1000) {
+    heroRank = "Ferro"
+
+  } else if (heroXP >= 1001 && heroXP <= 2000) {
+    heroRank = "Bronze"
+
+  } else if (heroXP >= 2001 && heroXP <= 5000) {
+    heroRank = "Prata"
+
+  } else if (heroXP >= 5001 && heroXP <= 7000) {
+    heroRank = "Ouro"
+
+  } else if (heroXP >= 7001 && heroXP <= 8000) {
+    heroRank = "Platina"
+
+  } else if (heroXP >= 8001 && heroXP <= 9000) {
+    heroRank = "Ascendente"
+
+  } else if (heroXP >= 9001 && heroXP <= 10000) {
+    heroRank = "Imortal"
+
+  } else {
+    heroRank = "Radiante" 
+  }
+}
+
+console.log("O Herói de nome " + heroName + " está no nível de " + heroRank)
